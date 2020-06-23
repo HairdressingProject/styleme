c7e69a13224d683e4cf6ad474c4358563901eae5
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2020-06-23 20:13:38 +0800

    finished sidebar

--- Frontend/js/database.js
+++ Frontend/js/database.js
@@ -1,5 +1,18 @@
 const logoutLink = document.getElementById('logout');
 const userName = document.getElementById('user-name');
+const resources = [
+    'colours',
+    'face_shape_links',
+    'face_shapes',
+    'hair_length_links',
+    'hair_lengths',
+    'hair_style_links',
+    'hair_styles',
+    'skin_tone_links',
+    'skin_tones',
+    'user_features',
+    'users'
+];
 
 document.addEventListener('DOMContentLoaded', async function () {
     const id = await authenticate();
@@ -17,4 +30,40 @@ document.addEventListener('DOMContentLoaded', async function () {
         e.preventDefault();
         await logout();
     });
+
+    fetchAll();
 });
+
+async function fetchResource(resourceName) {
+    const url = `https://localhost:5000/api/${resourceName}`;
+
+    const response = await fetch(url, {
+        method: 'GET',
+        credentials: 'include',
+        mode: 'cors',
+        headers: {
+            'Origin': 'http://localhost:5500'
+        }
+    });
+
+    const r = await response.json();
+
+    if (!response.ok) {
+        console.log(`Error when trying to fetch ${resourceName}`);
+        console.dir(r);
+        return;
+    }
+
+    return r;
+}
+
+async function fetchAll() {
+    let all = [];
+
+    for (let i = 0; i < resources.length; i++) {
+        const r = await fetchResource(resources[i]);
+        all.push(r);
+    }
+
+    console.dir(all);
+}
