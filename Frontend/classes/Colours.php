781dded37777f6619cfad5044f69db1377fa66bb
Diego Craveiro Chaves <20026893@tafe.wa.edu.au>
2020-06-27 22:54:18 +0800

    implemented handleResponse

--- Frontend/classes/Colours.php
+++ Frontend/classes/Colours.php
@@ -9,6 +9,13 @@
  * Description: add short description of file's purpose
  **********************************************************/
 
+require_once $_SERVER['DOCUMENT_ROOT'].'/helpers/actions/browse.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/helpers/actions/read.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/helpers/actions/edit.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/helpers/actions/add.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/helpers/actions/delete.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/helpers/utils.php';
+
 class Colours
 {
     public int $id;
@@ -22,25 +29,108 @@ class Colours
     }
 
     public function read() {
-        return readResource('colours', $this->id);
+        return readResource('colours', Utils::sanitiseField($this->id, FILTER_SANITIZE_NUMBER_INT));
     }
 
     public function edit() {
         return editResource('colours', array(
-            'Id' => $this->id,
-            'ColourName' => $this->colourName,
-            'ColourHash' => $this->colourHash
+            'Id' => Utils::sanitiseField($this->id, FILTER_SANITIZE_NUMBER_INT),
+            'ColourName' => Utils::sanitiseField($this->colourName, FILTER_SANITIZE_STRING),
+            'ColourHash' => Utils::sanitiseField($this->colourHash, FILTER_SANITIZE_STRING)
         ));
     }
 
     public function add() {
         return addResource('colours', array(
-            'ColourName' => $this->colourName,
-            'ColourHash' => $this->colourHash
+            'ColourName' => Utils::sanitiseField($this->colourName, FILTER_SANITIZE_STRING),
+            'ColourHash' => Utils::sanitiseField($this->colourHash, FILTER_SANITIZE_STRING)
         ));
     }
 
     public function delete() {
-        return deleteResource('colours', $this->id);
+        return deleteResource('colours', Utils::sanitiseField($this->id, FILTER_SANITIZE_NUMBER_INT));
+    }
+
+    /**
+     * Handles form submission for the users page
+     * @param  string  $method One of: 'POST', 'PUT' or 'DELETE'
+     * @return string|null Alert message
+     */
+    public function handleSubmit($method = 'POST')
+    {
+        switch ($method) {
+            case 'POST':
+                if (Utils::validateField('add_colourName', 'string') &&
+                    Utils::validateField('add_colourHash', 'string')) {
+                    $this->colourName = $_POST['add_colourName'];
+                    $this->colourHash = $_POST['add_colourHash'];
+
+                    $response = $this->add();
+
+                    return Utils::handleResponse(
+                        $response,
+                        [
+                            '400' => 'Invalid colour name / hash',
+                            '500' => 'Could not add colour. Please try again later.',
+                            '200' => 'Colour successfully added'
+                        ]
+                    );
+                }
+                else {
+                    return Utils::createAlert('Invalid colour name / hash fields', 'error');
+                }
+                break;
+
+            case 'PUT':
+                if (Utils::validateField('put_id', 'number')) {
+                    if (Utils::validateField('put_colourName', 'string')) {
+                        $this->id = $_POST['put_id'];
+                        $this->colourHash = $_POST['put_colourHash'];
+                        $this->colourName = $_POST['put_colourName'];
+
+                        $response = $this->edit();
+
+                        return Utils::handleResponse(
+                            $response,
+                            [
+                                '400' => 'Invalid colour name / hash',
+                                '404' => 'Colour not found',
+                                '500' => 'Could not update colour. Please try again later.',
+                                '200' => 'Colour successfully updated'
+                            ]
+                        );
+                    }
+                    else {
+                        return Utils::createAlert('Shape name cannot be empty', 'error');
+                    }
+                }
+                else {
+                    return Utils::createAlert('Invalid face shape ID', 'error');
+                }
+                break;
+
+            case 'DELETE':
+                if (Utils::validateField('delete_id', 'number')) {
+                    $this->id = $_POST['delete_id'];
+
+                    $response = $this->delete();
+
+                    return Utils::handleResponse(
+                        $response,
+                        [
+                            '404' => 'Colour not found',
+                            '500' => 'Could not delete colour. Please try again later.',
+                            '200' => 'Colour successfully deleted'
+                        ]
+                    );
+                }
+                else {
+                    return Utils::createAlert('Invalid colour ID', 'error');
+                }
+                break;
+
+            default:
+                return Utils::createAlert('Invalid request method', 'error');
+        }
     }
 }
