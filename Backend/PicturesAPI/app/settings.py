a0148ddd00192ebc59494b6a7fad35b8d604e90a
ggornes <gerardogornes@gmail.com>
2020-09-27 11:31:13 +0800

    created settings file to read env

---
+++ Backend/PicturesAPI/app/settings.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+env_path = Path().absolute() / '.env'
+load_dotenv(dotenv_path=env_path)
+
+DB_USER = os.getenv("DB_USER")
+DB_USER_PASSWORD = os.getenv("DB_USER_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+S3_AWS_ACCESS_KEY_ID = os.getenv("S3_AWS_ACCESS_KEY_ID")
+S3_AWS_SECRET_ACCESS_KEY = os.getenv("S3_AWS_SECRET_ACCESS_KEY")
