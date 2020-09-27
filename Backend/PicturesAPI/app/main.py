73bbc30f544163ee5a01913cb3e5369b586a14ef
ggornes <gerardogornes@gmail.com>
2020-09-27 16:19:10 +0800

    implemented detect_face_shape()

--- Backend/PicturesAPI/app/main.py
+++ Backend/PicturesAPI/app/main.py
@@ -15,8 +15,26 @@ def get_db():
 @app.post("/pictures/upload")
 async def save_picture(file: UploadFile = File(...), db: Session = Depends(get_db)):
     picture_service = services.PictureService()
-    file_info = picture_service.save_picture(file)
-    return {file_info}
+    
+    file_name = picture_service.save_picture(file)
+    # return {file_name}
+    
+    face_detected = picture_service.detect_face(file_name)
+    if face_detected is True:
+        # crop picture and save in preprocessed folder
+        picture_service.crop_picture(file_name)
+        # add picture to db
+        picture_info = picture_service.get_picture_info('pictures/original/', file_name)
+        print(picture_info)
+        new_picture = models.Picture(file_name=picture_info[1], file_path=picture_info[0], file_size=picture_info[2], height=picture_info[3], width=picture_info[4])
+        picture_actions = actions.PictureActions()
+        picture_actions.add_picture(db=db, picture=new_picture)
+        # detect face_shape
+        face_shape = picture_service.detect_face_shape(file_name)
+        return face_shape
+    else:
+        return {"No face detected"}
+    # return face_detected
 
 @app.get("/pictures/detect_face")
 async def detect_face(file_name: str, db: Session = Depends(get_db)):
--- Backend/PicturesAPI/app/services/picture_service.py
+++ Backend/PicturesAPI/app/services/picture_service.py
@@ -1,12 +1,14 @@
 import os
 import hashlib
 from datetime import datetime
-from app.settings import PICTURE_UPLOAD_FOLDER, PICTURE_PROCESSED_FOLDER
+from app.settings import PICTURE_UPLOAD_FOLDER, PICTURE_PROCESSED_FOLDER, FACE_SHAPE_RESULTS_PATH
 import pathlib
 import shutil
 import cv2
 from app.libraries.fmPyTorch.utils import Preprocess
+from app.libraries.Hair_Style_Recommendation.functions_only_save import make_face_df_save, find_face_shape
 import numpy as np
+import pandas as pd
 
 class PictureService:
     def save_picture(self, file):
@@ -70,7 +72,33 @@ class PictureService:
     
     def detect_face_shape(self, file_name):
         path = PICTURE_UPLOAD_FOLDER
+        save_path = FACE_SHAPE_RESULTS_PATH
         path_to_file = path + file_name
+
+        df = pd.DataFrame(columns = ['0','1','2','3','4','5','6','7','8','9','10','11',	'12',	'13',	'14',	'15',	'16','17',
+                             '18',	'19',	'20',	'21',	'22',	'23',	'24','25',	'26',	'27',	'28',	'29',
+                             '30',	'31',	'32',	'33',	'34',	'35',	'36',	'37',	'38',	'39',	'40',	'41',
+                             '42',	'43',	'44',	'45',	'46',	'47',	'48',	'49',	'50',	'51',	'52',	'53',
+                             '54',	'55',	'56',	'57',	'58',	'59',	'60',	'61',	'62',	'63',	'64',	'65',
+                             '66',	'67',	'68',	'69',	'70',	'71',	'72',	'73',	'74',	'75',	'76',	'77',
+                             '78',	'79',	'80',	'81',	'82',	'83',	'84',	'85',	'86',	'87',	'88',	'89',
+                             '90',	'91',	'92',	'93',	'94',	'95',	'96',	'97',	'98',	'99',	'100',	'101',
+                             '102',	'103',	'104',	'105',	'106',	'107',	'108',	'109',	'110',	'111',	'112',	'113',
+                             '114',	'115',	'116',	'117',	'118',	'119',	'120',	'121',	'122',	'123',	'124',	'125',
+                             '126',	'127',	'128',	'129',	'130',	'131',	'132',	'133',	'134',	'135',	'136',	'137',
+                             '138',	'139',	'140',	'141',	'142',	'143','A1','A2','A3','A4','A5','A6','A7','A8','A9',
+                            'A10','A11','A12','A13','A14','A15','A16','Width','Height','H_W_Ratio','Jaw_width','J_F_Ratio',
+                             'MJ_width','MJ_J_width'])
+        print(df)
+        photo = path+file_name
+        print(photo)
+        print(save_path)
+        file_num = 2035
+        make_face_df_save(photo, file_num, df)
+        print(df)
+        face_shape = find_face_shape(df, file_num)
+        print(face_shape)
+        return(face_shape[0])
 
 
     def crop_picture(self, file_name):
