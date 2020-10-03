2fa84d849c559c5f71df2d9ac2dd2e9308af2b2a
ggornes <gerardogornes@gmail.com>
2020-10-03 12:00:31 +0800

    added delete_picture() method (WIP) | added uppload model pictures option

--- Backend/PicturesAPI/app/actions/picture_actions.py
+++ Backend/PicturesAPI/app/actions/picture_actions.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+
+from app import models
 from app.models.picture import Picture
 from app.schemas.picture import PictureCreateUpdate
 
@@ -11,9 +13,27 @@ class PictureActions:
         db.commit()
         db.refresh(db_picture)
         return db_picture
-      
+
     def read_picture_by_id(self, db: Session, picture_id):
         return db.query(Picture).filter(Picture.id == picture_id).first()
 
+    # def read_picture_by_file_name(self, db: Session, file_name):
+    #     return db.query(Picture).filter(Picture.file_name == file_name)
+
     def read_pictures(self, db: Session, skip: int = 0, limit: int = 100):
         return db.query(Picture).offset(skip).limit(limit).all()
+
+    def delete_picture(self, db: Session, picture_id: int) -> models.Picture:
+        """
+        Deletes a picture record from the database
+        :param db: db session instance
+        :param picture_id: ID of the history entry to be updated
+        :return: History instance
+        """
+        picture_entry: models.Picture = db.query(models.Picture).filter(models.Picture.id == picture_id).first()
+
+        if picture_entry is not None:
+            db.delete(picture_entry)
+            db.commit()
+
+        return picture_entry
