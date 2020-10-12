0e243d5287b4f9201ef8b117c83b401d910c4d76
ggornes <gerardogornes@gmail.com>
2020-10-12 11:52:18 +0800

    changed folder structure and refactored app

---
+++ App/lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:app/views/pages/home.dart';
+import 'package:app/widgets/logo_banner.dart';
+import 'package:app/views/pages/sign_in.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hairdressing Project DEMO',
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(        
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Klavika'
+      ),
+      home: SplashScreen(),
+      routes: {
+        SignIn.routeName: (context) => SignIn(),
+        Home.routeName: (context) => Home()
+      },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key key,}) : super(key: key);
+
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+
+    Timer(Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacementNamed(SignIn.routeName);
+      Navigator.of(context).pushReplacementNamed(Home.routeName);
+    });
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Align(
+            alignment: Alignment.center,
+            child: LogoBanner(logoPath: 'assets/icons/woman.png')
+          )
+        )
+      ),
+    );
+  }
+}
---
+++ App/lib/views/pages/sign_in.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:app/widgets/logo_banner.dart';
+
+
+class SignIn extends StatefulWidget {
+
+  static final String routeName = '/signInRoute';
+
+  SignIn({Key key}) : super(key: key);
+
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(50.0, 50.0, 50.0, 0.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              LogoBanner(logoPath: 'assets/icons/woman.png'),
+              Text(
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+              Form(
+                child: Column(
+                  children: <Widget>[
+                    Padding(padding: const EdgeInsets.only(top: 35.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Username or email'
+                        ),
+                      ),
+                    ),
+                    Padding(padding: const EdgeInsets.only(top: 35.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password'
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(padding: const EdgeInsets.only(top: 35),
+                child: Text("Forgot password?")
+              ),
+              Padding(padding: const EdgeInsets.only(top: 35.0),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: Text('Sign In'),
+                  color: Colors.red,
+                  height: MediaQuery.of(context).size.height / 15,
+                  minWidth: double.infinity,
+                ),
+              ),
+              Padding(padding: const EdgeInsets.only(top: 60),
+                child: Text("Not registered yet? Create an account")
+              ),
+            ],
+          ),
+        )
+        
+      ),
+    );
+  }
+}
