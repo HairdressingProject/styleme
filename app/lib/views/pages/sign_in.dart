b779b5d7882b12e3ea07838f9b6ea930fd24fa4f
ggornes <gerardogornes@gmail.com>
2020-10-13 12:27:16 +0800

    Implementing sign in form validations (WIP)

--- app/lib/views/pages/sign_in.dart
+++ app/lib/views/pages/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:app/widgets/logo_banner.dart';
+import 'package:app/widgets/sign_in_form.dart';
 
 class SignIn extends StatefulWidget {
   static final String routeName = '/signInRoute';
@@ -22,42 +23,43 @@ class _SignInState extends State<SignIn> {
           children: <Widget>[
             LogoBanner(logoPath: 'assets/icons/woman.png'),
             Text('Sign In', style: Theme.of(context).textTheme.headline1),
-            Form(
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 35.0),
-                    child: TextFormField(
-                      decoration:
-                          InputDecoration(labelText: 'Username or email'),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 35.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Password'),
-                    ),
-                  )
-                ],
-              ),
-            ),
+            // Form(
+            //   child: Column(
+            //     children: <Widget>[
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 35.0),
+            //         child: TextFormField(
+            //           decoration:
+            //               InputDecoration(labelText: 'Username or email'),
+            //         ),
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 35.0),
+            //         child: TextFormField(
+            //           decoration: InputDecoration(labelText: 'Password'),
+            //         ),
+            //       )
+            //     ],
+            //   ),
+            // ),
+            SignInForm(),
             Padding(
                 padding: const EdgeInsets.only(top: 35),
                 child: Text("Forgot password?")),
-            Padding(
-              padding: const EdgeInsets.only(top: 35.0),
-              child: MaterialButton(
-                onPressed: () {},
-                child: Text(
-                  'Sign in',
-                  style: Theme.of(context).textTheme.bodyText1.copyWith(
-                      color: Colors.white, fontWeight: FontWeight.w700),
-                ),
-                color: Color.fromARGB(255, 74, 169, 242),
-                height: MediaQuery.of(context).size.height / 15,
-                minWidth: double.infinity,
-              ),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.only(top: 35.0),
+            //   child: MaterialButton(
+            //     onPressed: () {},
+            //     child: Text(
+            //       'Sign in',
+            //       style: Theme.of(context).textTheme.bodyText1.copyWith(
+            //           color: Colors.white, fontWeight: FontWeight.w700),
+            //     ),
+            //     color: Color.fromARGB(255, 74, 169, 242),
+            //     height: MediaQuery.of(context).size.height / 15,
+            //     minWidth: double.infinity,
+            //   ),
+            // ),
             Padding(
                 padding: const EdgeInsets.only(top: 60),
                 child: Text("Not registered yet? Create an account")),
