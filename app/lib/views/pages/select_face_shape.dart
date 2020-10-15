2757ae242b43fdc612252e99d1011ee26e00a757
Diego C <diego.cc.github@gmail.com>
2020-10-15 13:58:37 +0800

    fixing conflicts

--- app/lib/views/pages/select_face_shape.dart
+++ app/lib/views/pages/select_face_shape.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:app/views/layout.dart';
-import 'package:app/widgets/face_card.dart';
-import 'package:app/widgets/custom_radio_item.dart';
 
 class SelectFaceShape extends StatelessWidget {
   static final String routeName = '/selectFaceShapeRoute';
@@ -13,34 +11,6 @@ class SelectFaceShape extends StatelessWidget {
         title: 'Style Me',
         header: 'Select Face Shape',
         drawerItems: buildDefaultDrawerItems(context),
-        body: Scrollbar(
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child:
-              CustomRadio()
-              //Column(children: [CustomRadio()],)
-              // Column(
-              //   children: [
-              //     CustomRadio(),
-              //     Padding(
-              //       padding: const EdgeInsets.symmetric(vertical: 40.0),
-              //       child: MaterialButton(
-              //         disabledColor: Colors.grey[600],
-              //         disabledTextColor: Colors.white,
-              //         onPressed: () {
-              //           // send request to authenticate data with Users API
-              //           Scaffold.of(context)
-              //               .showSnackBar(SnackBar(content: Text('Processing Data')));
-              //         },
-              //         color: Color.fromARGB(255, 74, 169, 242),
-              //         minWidth: double.infinity,
-              //         child: Text('Sign up'),
-              //       ),
-              //     ),
-              //   ],
-              // )
-          )
-        )
-      );
+        body: Text('Select face shape'));
   }
 }
