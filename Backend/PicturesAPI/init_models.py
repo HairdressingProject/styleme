f3f1eea7af00a1616c198c69df6108f53e9a4a3c
ggornes <gerardogornes@gmail.com>
2020-10-20 12:18:30 +0800

    fixed script

--- Backend/PicturesAPI/init_models.py
+++ Backend/PicturesAPI/init_models.py
@@ -19,10 +19,11 @@ for hl in hair_lengths_dirs:
     for hs in os.listdir(hl):
         for root, directories, files in os.walk(hl, topdown=False):
             for name in files:
-                file_info = (os.path.join(root, name).split('\\')[9:12])
+                dirs = len(os.path.join(root, name).split('\\'))
+                file_info = (os.path.join(root, name).split('\\')[dirs-3:dirs])
                 file_request = path + '\\' + file_info[0] + '\\' + file_info[1] + '\\' + file_info[2]
                 file = {"file": open(file_request, 'rb')}
                 print(file)
                 url = "http://localhost:8000/models?hair_length=" + file_info[0] + "&hair_style=" + file_info[1]
                 print(url)
-                requests.post(url, headers, files=file)
+                # requests.post(url, headers, files=file)
