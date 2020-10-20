71ec91c236fb58da82fc0545791d62c43d874352
ggornes <gerardogornes@gmail.com>
2020-10-20 14:15:23 +0800

    fixed init models script

--- Backend/PicturesAPI/init_models.py
+++ Backend/PicturesAPI/init_models.py
@@ -1,6 +1,6 @@
 import os
 import requests
-
+i = 0
 headers = {
     'accept': 'application/json',
     'Content-Type': 'multipart/form-data'
@@ -16,14 +16,18 @@ for hl in hair_lengths:
     hair_lengths_dirs.append(new_path)
 
 for hl in hair_lengths_dirs:
-    for hs in os.listdir(hl):
-        for root, directories, files in os.walk(hl, topdown=False):
-            for name in files:
-                dirs = len(os.path.join(root, name).split('\\'))
-                file_info = (os.path.join(root, name).split('\\')[dirs-3:dirs])
-                file_request = path + '\\' + file_info[0] + '\\' + file_info[1] + '\\' + file_info[2]
-                file = {"file": open(file_request, 'rb')}
-                print(file)
-                url = "http://localhost:8000/models?hair_length=" + file_info[0] + "&hair_style=" + file_info[1]
-                print(url)
-                # requests.post(url, headers, files=file)
+    print("************************************************************************************************")
+    print(hl)
+    print("************************************************************************************************")
+    for root, directories, files in os.walk(hl, topdown=False):
+        for name in files:
+            i += 1
+            dirs = len(os.path.join(root, name).split('\\'))
+            file_info = (os.path.join(root, name).split('\\')[dirs-3:dirs])
+            file_request = path + '\\' + file_info[0] + '\\' + file_info[1] + '\\' + file_info[2]
+            file = {"file": open(file_request, 'rb')}
+            print(file)
+            url = "http://localhost:8000/models?hair_length=" + file_info[0] + "&hair_style=" + file_info[1]
+            print(url)
+            # requests.post(url, headers, files=file)
+            print(i)
