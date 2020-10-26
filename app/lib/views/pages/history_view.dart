ebd17d24ba64e23adaf007252d24cbf0b97b8cd5
Diego C <diego.cc.github@gmail.com>
2020-10-26 14:08:00 +0800

    implemented HistoryView (compare results)

---
+++ app/lib/views/pages/history_view.dart
@@ -0,0 +1,161 @@
+import 'package:app/models/picture.dart';
+import 'package:app/services/constants.dart';
+import 'package:app/services/pictures.dart';
+import 'package:app/widgets/comparison_picture.dart';
+import 'package:app/widgets/preview.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HistoryView extends StatelessWidget {
+  final Picture originalPicture;
+  final List<Picture> historyPictures;
+  final String origin;
+  final String userToken;
+  final ScrollController _scrollController = ScrollController();
+
+  HistoryView(
+      {Key key,
+      @required this.originalPicture,
+      @required this.historyPictures,
+      this.origin = ADMIN_PORTAL_URL,
+      @required this.userToken})
+      : super(key: key);
+
+  _onPicturePreview({@required BuildContext context, @required int pictureId}) {
+    final pictureUrl = '${PicturesService.picturesUri}/file/$pictureId';
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => Preview(
+                  previewPictureUrl: pictureUrl,
+                  userToken: userToken,
+                  origin: origin,
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final originalPictureUrl =
+        '${PicturesService.picturesUri}/file/${originalPicture.id.toString()}';
+
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 96, 96, 96),
+        appBar: AppBar(
+            title: Text(
+          'History',
+          style: TextStyle(fontFamily: 'Klavika'),
+        )),
+        body: Scrollbar(
+          controller: _scrollController,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              alignment: Alignment.center,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  GestureDetector(
+                      onTap: () {
+                        _onPicturePreview(
+                            context: context, pictureId: originalPicture.id);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 30.0, horizontal: 50.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Original',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Klavika',
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w700,
+                                  letterSpacing: 0.8),
+                            ),
+                            const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10.0)),
+                            Image.network(
+                              originalPictureUrl,
+                              headers: {
+                                "Origin": origin,
+                                "Authorization": "Bearer $userToken"
+                              },
+                            )
+                          ],
+                        ),
+                      )),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10.0),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                        crossAxisCount: 2,
+                        padding: const EdgeInsets.all(20.0),
+                        physics: NeverScrollableScrollPhysics(),
+                        mainAxisSpacing: 5.0,
+                        children:
+                            List.generate(historyPictures.length, (index) {
+                          final currentPicture = historyPictures[index];
+                          final parsedDateCreated =
+                              DateTime.parse(currentPicture.dateCreated);
+
+                          final pictureDateCreated =
+                              '${parsedDateCreated.day}/${parsedDateCreated.month}/${parsedDateCreated.year} at ${parsedDateCreated.hour}:${parsedDateCreated.minute}:${parsedDateCreated.second}';
+                          final pictureUrl = Uri.encodeFull(
+                              '${PicturesService.picturesUri}/file/${currentPicture.id.toString()}');
+
+                          return GestureDetector(
+                              onTap: () {
+                                _onPicturePreview(
+                                    context: context,
+                                    pictureId: currentPicture.id);
+                              },
+                              child: Column(
+                                children: [
+                                  Text(
+                                    pictureDateCreated,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Klavika',
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.w700,
+                                      letterSpacing: 0.5,
+                                    ),
+                                  ),
+                                  const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 10.0)),
+                                  Expanded(
+                                    child: Image.network(
+                                      pictureUrl,
+                                      headers: {
+                                        "Origin": origin,
+                                        "Authorization": "Bearer $userToken"
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ));
+
+                          /* ComparisonPicture(
+                            titleStyle: TextStyle(
+                                fontFamily: 'Klavika',
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                letterSpacing: 0.8),
+                            title: pictureDateCreated,
+                            pictureUrl: pictureUrl,
+                            userToken: userToken,
+                            origin: origin,
+                          ); */
+                        })),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
--- app/lib/widgets/comparison_picture.dart
+++ app/lib/widgets/comparison_picture.dart
@@ -3,71 +3,124 @@ import 'package:flutter/material.dart';
 
 class ComparisonPicture extends StatelessWidget {
   final String title;
+  final TextStyle titleStyle;
   final String origin;
   final String userToken;
   final String pictureUrl;
   final bool sideBySide;
+  final bool interactive;
 
   const ComparisonPicture(
       {Key key,
       @required this.title,
+      this.titleStyle,
       @required this.pictureUrl,
       this.origin = ADMIN_PORTAL_URL,
       @required this.userToken,
-      this.sideBySide = true})
+      this.sideBySide = true,
+      this.interactive = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return sideBySide
         ? Expanded(
-            child: Column(
-              children: [
-                Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text(
-                      title,
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
-                          fontFamily: 'Klavika',
-                          letterSpacing: 1.0),
-                    )),
-                Image.network(
-                  pictureUrl,
-                  headers: {
-                    "Origin": origin,
-                    "Authorization": "Bearer $userToken"
-                  },
-                )
-              ].where((element) => element != null).toList(),
-            ),
+            child: !interactive
+                ? Column(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Text(
+                            title,
+                            style: titleStyle ??
+                                TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white,
+                                    fontFamily: 'Klavika',
+                                    letterSpacing: 1.0),
+                          )),
+                      Image.network(
+                        pictureUrl,
+                        headers: {
+                          "Origin": origin,
+                          "Authorization": "Bearer $userToken"
+                        },
+                      )
+                    ].where((element) => element != null).toList(),
+                  )
+                : Stack(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Text(
+                            title,
+                            style: titleStyle ??
+                                TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.white,
+                                    fontFamily: 'Klavika',
+                                    letterSpacing: 1.0),
+                          )),
+                      Image.network(
+                        pictureUrl,
+                        headers: {
+                          "Origin": origin,
+                          "Authorization": "Bearer $userToken"
+                        },
+                      )
+                    ].where((element) => element != null).toList(),
+                  ),
           )
         : Container(
-            child: Column(
-              children: [
-                Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                    child: Text(
-                      title,
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
-                          color: Colors.white,
-                          fontFamily: 'Klavika',
-                          letterSpacing: 1.0),
-                    )),
-                Expanded(
-                    child: Image.network(
-                  pictureUrl,
-                  headers: {
-                    "Origin": origin,
-                    "Authorization": "Bearer $userToken"
-                  },
-                ))
-              ].where((element) => element != null).toList(),
-            ),
+            child: !interactive
+                ? Column(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Text(
+                            title,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                fontFamily: 'Klavika',
+                                letterSpacing: 1.0),
+                          )),
+                      Expanded(
+                          child: Image.network(
+                        pictureUrl,
+                        headers: {
+                          "Origin": origin,
+                          "Authorization": "Bearer $userToken"
+                        },
+                      ))
+                    ].where((element) => element != null).toList(),
+                  )
+                : Stack(
+                    children: [
+                      Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Text(
+                            title,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                fontFamily: 'Klavika',
+                                letterSpacing: 1.0),
+                          )),
+                      Expanded(
+                          child: Image.network(
+                        pictureUrl,
+                        headers: {
+                          "Origin": origin,
+                          "Authorization": "Bearer $userToken"
+                        },
+                      ))
+                    ].where((element) => element != null).toList(),
+                  ),
           );
   }
 }
