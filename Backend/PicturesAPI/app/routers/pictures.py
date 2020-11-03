4446ad23d9682178409bc1c5508159ec2d73ee27
Diego C <diego.cc.github@gmail.com>
2020-11-03 17:39:19 +0800

    GET /pictures/change_hair_colour now returns picture obj

--- Backend/PicturesAPI/app/routers/pictures.py
+++ Backend/PicturesAPI/app/routers/pictures.py
@@ -1,4 +1,5 @@
 import json
+import os
 from typing import List, Optional, Union
 
 import requests
@@ -118,7 +119,8 @@ async def read_picture_file(picture_id: int, db: Session = Depends(get_db)):
     selected_picture = picture_actions.read_picture_by_id(picture_id=picture_id, db=db)
     if selected_picture:
         file_path = selected_picture.file_path + selected_picture.file_name
-        return FileResponse(file_path)
+        if os.path.exists(file_path):
+            return FileResponse(file_path)
     raise HTTPException(status_code=404, detail='Picture file not found')
 
 
@@ -259,7 +261,10 @@ async def change_hair_colour(picture_id: int, colour: str, r: int, b: int, g: in
                     hair_colour_entry: models.HairColour = hair_colour_actions.get_hair_colour_by_id(db=db,
                                                                                                      hair_colour_id=hair_colour.id)
 
-                    return {'history_entry': history_entry, 'picture': mod_pic, 'hair_colour': hair_colour_entry}
+                    # get new picture with modified hair colour
+                    new_pic: models.Picture = picture_actions.read_picture_by_id(db=db, picture_id=history_entry.picture_id)
+
+                    return {'history_entry': history_entry, 'picture': new_pic, 'hair_colour': hair_colour_entry}
                 raise HTTPException(status_code=404,
                                     detail='No hair colour record associated with this colour name was found')
         except:
@@ -348,7 +353,7 @@ async def change_hairstyle(user_picture_id: Optional[int] = None, model_picture_
             raise HTTPException(status_code=404, detail='No user associated with this picture was found')
         except Exception as e:
             raise HTTPException(status_code=422,
-                                detail=f'Could not apply hair style swap. Please try another image.\n\nException: {e}')
+                                detail=f'Could not apply hair style swap. Please try another image. Exception: {e}')
 
     raise HTTPException(status_code=404, detail='No user picture or model picture associated with these IDs were found')
 
