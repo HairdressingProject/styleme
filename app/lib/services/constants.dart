0c19d3cb2632ee018526b5139fe246e6dd54e682
Diego C <diego.cc.github@gmail.com>
2020-11-20 14:32:10 +0800

    updated URL constants to https

--- app/lib/services/constants.dart
+++ app/lib/services/constants.dart
@@ -1,8 +1,8 @@
 /// Base URL that connects to the Users API
-const String USERS_API_URL = 'http://api.styleme.best';
+const String USERS_API_URL = 'https://api.styleme.best';
 
 /// Base URL that connects to the Pictures API
-const String PICTURES_API_URL = 'http://api.styleme.best';
+const String PICTURES_API_URL = 'https://api.styleme.best';
 
 /// Base URL of the Admin Portal, sent in `Origin` request headers
 const String ADMIN_PORTAL_URL = 'https://styleme.best';
